1648e997ce78e756677cbc162cb9f1edb9cddcd6
Lain <chiyomiziwari@gmail.com>
2026-07-15 12:05:06 +0700

    Introduce Specification pattern for repository queries
    Added ISpecification and BaseSpecification to encapsulate query logic such as criteria, includes, ordering, and paging. Updated IRepository to support specifications and implemented a generic Repository using SpecificationEvaluator for Entity Framework integration. Cleaned up project folders to reflect the new structure.

---
+++ src/Core/Agriculture.Domain/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Agriculture.Domain.Specifications;
+
+namespace Agriculture.Domain.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> ToListAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<TEntity>> ToListAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+        Task<TEntity?> FindAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ src/External/Agriculture.Persistence/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Agriculture.Domain.Repositories;
+using Agriculture.Domain.Specifications;
+using Agriculture.Persistence.Contexts;
+using Agriculture.Persistence.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agriculture.Persistence.Repositories
+{
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly AgricultureDbContext _context;
+        protected readonly DbSet<TEntity> Entity;
+
+        public Repository(AgricultureDbContext context)
+        {
+            _context = context;
+            Entity = _context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> ToListAsync(CancellationToken cancellationToken = default)
+        {
+            return await Entity.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<TEntity>> ToListAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default)
+        {
+            var query = SpecificationEvaluator
+                .GetQuery(Entity, specification);
+            return await query.ToListAsync(cancellationToken);
+        }
+
+        public async Task<TEntity?> FindAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default)
+        {
+            var query = SpecificationEvaluator
+                .GetQuery(Entity, specification);
+            return await query.FirstOrDefaultAsync(cancellationToken);
+        }
+        public void Add(TEntity entity)
+        {
+            Entity.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Entity.Update(entity);
+        }
+    }
+}
